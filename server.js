ae062994c49a82fb5e3f07a05f9d5fc164b2da07
tatianasnook <tatsnook@gmail.com>
2024-03-26 17:05:05 -0500

    changes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 mongoose.set("strictQuery", false)
-mongoose.connect('')
+mongoose
+.connect(process.env.MONGODB_LINK)
+.then(() => console.log('WE WERE CONNECTED TO MONGO'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.post('/', (req, res) => {
+  let newMessage = new Message({
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message
+  })
+  newMessage.save()
+  res.sendFile(__dirname + '/answer.html')
+})
+
+app.listen(8000, () => {
+  console.log('server is listening on PORT 8000')
+})
